5d9e7a69caec3361d875fa5750538e7e80d45fff
Liudmyla_Kobialko <Liudmyla_Kobialko@EPUAVINW0228>
2023-10-19 18:49:05 +0300

    Add project files.

---
+++ Task1/Utilities.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Sorts an array in ascending order using bubble sort.
+        /// </summary>
+        /// <param name="numbers">Numbers to sort.</param>
+        public static void Sort(int[] numbers)
+        {
+            if (numbers == null)
+            {
+               throw new ArgumentNullException(nameof(numbers));
+            }
+            int temp;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] >= numbers[j])
+                    {
+                        continue;
+                    }
+                    temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// based on a <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="products">Products used for searching.</param>
+        /// <param name="predicate">Product predicate.</param>
+        /// <returns>If match found then returns index of product in <paramref name="products"/>
+        /// otherwise -1.</returns>
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            if (products == null)
+            {  
+                throw new ArgumentNullException(nameof(products)); 
+            }
+
+            for (int i = products.Length - 1; i >= 0; i--)
+            {
+                var product = products[i];
+                if (predicate(product))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
